d91196c5515859d8d6060837be135a2c71c2cc7d
Mansoor <mansoorelahi777@gmail.com>
2016-03-27 16:32:04 +0530

    TASK:3 add the recent scope

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,4 +4,6 @@ class Song < ActiveRecord::Base
   
   validates :name, presence: true, length: { in: 5..50 }
   validates :album_id, :artist_id, presence: true
+  scope :recent, ->(n=nil) {where("created_at >= ?", 10.months.ago).last(n) }
+
 end
